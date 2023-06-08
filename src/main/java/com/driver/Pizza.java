68cf1579645945f07ece80cccfb3fe51079c7638
Akash <abd.akashraj519@gmail.com>
2023-06-08 15:41:40 +0530

    pizza billing sumitted in spring basics

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,30 @@ package com.driver;
 public class Pizza {
 
     private int price;
+    private int toppings;
+
     private Boolean isVeg;
     private String bill;
+
+    private boolean isExtraCheeseAdded;
+    private boolean isExtraToppingsAdded;
+    private boolean isTakeAway;
+    private boolean isBillingFinished;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            this.price =300;
+            this.toppings = 70;
+        }
+        else {
+            this.price = 400;
+            this.toppings=120;
+        }
+
+        bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +35,47 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded)
+        {
+            this.price += 80;
+            isExtraCheeseAdded = true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingsAdded)
+        {
+            this.price +=this.toppings;
+            isExtraToppingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway)
+        {
+            this.price += 20;
+            isTakeAway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!isBillingFinished) {
+            if(isExtraCheeseAdded)
+                bill+="Extra Cheese Added: "+80+"\n";
+            if(isExtraToppingsAdded)
+                bill+="Extra Toppings Added: "+this.toppings+"\n";
+            if(isTakeAway)
+            {
+                bill+="Paperbag Added: "+20+"\n";
+            }
+            bill+="Total Price: "+this.price+"\n";
+            isBillingFinished = true;
+            return this.bill;
+        }
+            return "";
     }
 }
